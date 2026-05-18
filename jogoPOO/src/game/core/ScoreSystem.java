be059146f71b34b70fe1ee79ae34b6aea081fd0a
Gabriela Bandeira <gabrielabdsantana@gmail.com>
2026-05-18 01:03:39 -0300

    Refactor ScoreSystem to manage player score

--- jogoPOO/src/game/core/ScoreSystem.java
+++ jogoPOO/src/game/core/ScoreSystem.java
@@ -1,5 +1,18 @@
 package game.core;
 
+//Gerencia a pontuação do jogador durante o jogo
 public class ScoreSystem {
-    
+    private int pontuacaoTotal;
+
+    public ScoreSystem() {
+        this.pontuacaoTotal = 0;
+    }
+
+    public void adicionarPontos(int pontos) {
+        this.pontuacaoTotal += pontos;
+    }
+
+    public int getPontuacaoTotal() {
+        return pontuacaoTotal;
+    }
 }
