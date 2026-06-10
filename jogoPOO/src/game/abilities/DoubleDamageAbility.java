b2040690403fb2345baf762629bc2a72d43e6637
Gabriela Bandeira <gabrielabdsantana@gmail.com>
2026-06-10 00:46:30 -0300

    Add DoubleDamageAbility class for enhanced damage

---
+++ jogoPOO/src/game/abilities/DoubleDamageAbility.java
@@ -0,0 +1,18 @@
+package game.abilities;
+
+import game.characters.Player;
+import game.characters.Enemy;
+
+public class DoubleDamageAbility extends Ability {
+    public DoubleDamageAbility() {
+        super("Cataclismo (Dobrar Dano)", 1); // 1 uso (é muito forte)
+    }
+
+    @Override
+    public void ativar(Player heroi, Enemy akumatizado) {
+        System.out.println("\n🐾 CATACLISMO!");
+        System.out.println("O poder da destruição foi ativado! Seu próximo acerto causará o dobro de dano no akumatizado!");
+        // heroi.getPersonagemSelecionado().setDobrarProximoDano(true); // Precisará de uma flag booleana na classe Character
+        consumirUso();
+    }
+}
