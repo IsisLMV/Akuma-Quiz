b75a8408d63806d5f15964d4663392fd25dec515
Gabriela Bandeira <gabrielabdsantana@gmail.com>
2026-06-10 00:48:23 -0300

    Create HintAbility.java

---
+++ jogoPOO/src/game/abilities/HintAbility.java
@@ -0,0 +1,18 @@
+package game.abilities;
+
+import game.characters.Player;
+import game.characters.Enemy;
+
+public class HintAbility extends Ability {
+    public HintAbility() {
+        super("Miragem (Criar Ilusão nas Alternativas)", 2);
+    }
+
+    @Override
+    public void ativar(Player heroi, Enemy akumatizado) {
+        System.out.println("\n🦊 MIRAGEM!");
+        System.out.println("Uma ilusão escondeu algumas das alternativas erradas!");
+        // Lógica a ser conectada com a Question atual na rodada
+        consumirUso();
+    }
+}
