5323d35103ce7274ad33a5481ac3a0031a442709
Gabriela Bandeira <gabrielabdsantana@gmail.com>
2026-06-10 00:48:52 -0300

    Add SecondChanceAbility class for retry mechanism

---
+++ jogoPOO/src/game/abilities/SecondChanceAbility.java
@@ -0,0 +1,18 @@
+package game.abilities;
+
+import game.characters.Player;
+import game.characters.Enemy;
+
+public class SecondChanceAbility extends Ability {
+    public SecondChanceAbility() {
+        super("Segunda Chance (Tentar Novamente)", 2);
+    }
+
+    @Override
+    public void ativar(Player heroi, Enemy akumatizado) {
+        System.out.println("\n🐍 SEGUNDA CHANCE!");
+        System.out.println("O ponto de salvamento temporal foi ativado. Se você errar, o tempo voltará para você tentar de novo.");
+        // heroi.setSegundaChance(true);
+        consumirUso();
+    }
+}
