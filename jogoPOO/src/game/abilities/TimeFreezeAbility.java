1c038c4ba3ed14bb4e33b4ff9dbfe4192a20ce62
Gabriela Bandeira <gabrielabdsantana@gmail.com>
2026-06-10 00:47:36 -0300

    Add TimeFreezeAbility class for time manipulation

---
+++ jogoPOO/src/game/abilities/TimeFreezeAbility.java
@@ -0,0 +1,18 @@
+package game.abilities;
+
+import game.characters.Player;
+import game.characters.Enemy;
+
+public class TimeFreezeAbility extends Ability {
+    public TimeFreezeAbility() {
+        super("Ferroada (Paralisar Temporizador)", 1);
+    }
+
+    @Override
+    public void ativar(Player heroi, Enemy akumatizado) {
+        System.out.println("\n🐝 FERROADA!");
+        System.out.println("O tempo foi paralisado! Você tem tempo infinito para responder a esta pergunta.");
+        // Lógica a ser conectada com a TimedQuestion
+        consumirUso();
+    }
+}
